ec3fd808b31bce17659294c43a42bb353257c7ab
Nico7522 <nico.daddabbo7100#gmail.com>
2023-05-08 16:52:28 +0200

    08/05

--- src/components/recipe/recipe-component.jsx
+++ src/components/recipe/recipe-component.jsx
@@ -1,7 +1,18 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useQuery } from "react-query"
+
 export default function Recipe({id, name, description, ingredients}){
+useEffect(() => {
+    const r = axios.get('http://localhost:8080/api/recipe/react/'+id).then((r) => (r))
+
+    return {r}
+}, [])
+   
     return(
 
         <div key={id} className='mx-auto mt-5 w-10/12 border-4 border-indigo-500/100'>
+            {console.log(r)}
             <h2>{name}</h2>
             <p>{description}</p>
             {ingredients.map(i => (
