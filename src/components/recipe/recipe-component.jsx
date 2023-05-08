b39478a0cc4fdab324a84e000e53de1efde8e9dd
Nico7522 <nico.daddabbo7100#gmail.com>
2023-05-08 19:23:04 +0200

    08/05

--- src/components/recipe/recipe-component.jsx
+++ src/components/recipe/recipe-component.jsx
@@ -1,18 +1,31 @@
 import axios from "axios"
+import { useState } from "react";
 import { useEffect } from "react"
 import { useQuery } from "react-query"
 
-export default function Recipe({id, name, description, ingredients}){
-useEffect(() => {
-    const r = axios.get('http://localhost:8080/api/recipe/react/'+id).then((r) => (r))
+export default function Recipe({id, name, description, ingredients, reactions}){
+    
+    // const { data } = useQuery(['Recipes', id], {
+    //     queryFn: () => axios.get('http://localhost:8080/api/recipe/react/'+ id)
+    // })
+    console.log(reactions);
+    let like = 0;
+    let tasty = 0;
 
-    return {r}
-}, [])
-   
+    reactions.forEach((r) => {
+        if (r.reaction === "like") {
+            like+=1;
+        }
+        if (r.reaction === "tasty") {
+            tasty+=1;
+        }
+    })
+    
+ 
     return(
 
         <div key={id} className='mx-auto mt-5 w-10/12 border-4 border-indigo-500/100'>
-            {console.log(r)}
+    
             <h2>{name}</h2>
             <p>{description}</p>
             {ingredients.map(i => (
@@ -22,6 +35,10 @@ useEffect(() => {
                     <p> Proteins : {i.proteins}</p>
                     <p> Fats : {i.fats}</p>
                     <p> Carbohydrates : {i.carbohydrates}</p>
+                    <p>❤ : {like}</p>
+                    <p>🤤 : {tasty}</p>
+                
+                    
                 </div>
             ))}
         </div>
