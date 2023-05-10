7510b6694f5f387cce00b0b25ffcf8004a1091ca
Nico7522 <nico.daddabbo7100#gmail.com>
2023-05-10 20:40:10 +0200

    test multipleselect

---
+++ API/ingredient.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+import { useQuery } from "react-query"
+import { useQueries } from "react-query"
+
+
+export const useFetchIngredient = () => {
+    return useQuery('Ingredients', async () => {
+       const { data } = await axios.get('http://localhost:8080/api/ingredient/')
+        return data.results
+    })
+}
--- src/containers/recipes/recipe-form.jsx
+++ src/containers/recipes/recipe-form.jsx
@@ -1,14 +1,82 @@
-import React, { useState } from "react";
+// import React, { useState, useEffect } from "react";
+// import { useForm, Controller } from "react-hook-form";
+// import { MultiSelect } from "react-multi-select-component";
+// import { useFetchIngredient } from "../../../API/ingredient";
+
+// export default function RecipeForm() {
+//   const [options, setOptions] = useState([]);
+//   const { isLoading, data } = useFetchIngredient();
+
+//   useEffect(() => {
+//     if (!isLoading) {
+//       setOptions(data);
+//     }
+//   }, [isLoading, data]);
+
+//   const {
+//     handleSubmit,
+//     control,
+//     register,
+//     reset,
+//     formState: { errors },
+//     getValues,
+//   } = useForm({});
+  
+//   const submit = () => {
+//     const values = getValues();
+//     const data = values.data.name.map((element) => element.name);
+//     console.log("body =>", data);
+//   };
+  
+//   return (
+//     <form onSubmit={handleSubmit(submit)}>
+//       <Controller
+//         control={control}
+//         name="data.name"
+//         render={({ field: { onChange, value } }) => (
+//           <MultiSelect
+//           options={data.map(({ name, _id }) => ({ label: name, value: _id, name: name }))}
+//           value={value ? value : []}
+//           onChange={(selected) => {
+//             const selectedNames = selected.map((option) => option.name);
+//             const previousNames = value ? value.map((option) => option.name) : [];
+//             const newValue = data.filter((option) => {
+//               if (selectedNames.includes(option.name)) {
+//                 return true;
+//               }
+//               if (previousNames.includes(option.name)) {
+//                 return false;
+//               }
+//               return option.is_default;
+//             });
+//             onChange(newValue);
+//           }}
+//             labelledBy="Select"
+//             disableSearch
+//             hasSelectAll={false}
+//           />
+//         )}
+//       />
+//       <button type="submit">TEST</button>
+//     </form>
+//   );
+// }
+
+
+import React, { useState, useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { MultiSelect } from "react-multi-select-component";
+import { useFetchIngredient } from "../../../API/ingredient";
 
 export default function RecipeForm() {
-  const [selected, setSelected] = useState([]);
-  const options = [
-    { label: "Grapes 🍇", value: "grapes" },
-    { label: "Mango 🥭", value: "mango" },
-    { label: "Strawberry 🍓", value: "strawberry", disabled: true },
-  ];
+  const [options, setOptions] = useState([]);
+  const { isLoading, data } = useFetchIngredient();
+
+  useEffect(() => {
+    if (!isLoading) {
+      setOptions(data);
+    }
+  }, [isLoading, data]);
 
   const {
     handleSubmit,
@@ -16,31 +84,40 @@ export default function RecipeForm() {
     register,
     reset,
     formState: { errors },
-    onChange,
+    getValues,
   } = useForm({});
-  const submit = ({ data }) => {
-    console.log("data =>", data);
-    const donnee = data?.options.map((element) => element.value);
-    const body = JSON.stringify({ ...data, test: donnee });
-    console.log("body =>", body);
+  
+  const submit = () => {
+    const values = getValues();
+    const data = values.data.name.map((element) => element.name);
+    console.log("body =>", data);
   };
+  
   return (
     <form onSubmit={handleSubmit(submit)}>
       <Controller
         control={control}
-        name="options"
+        name="data.name"
         render={({ field: { onChange, value } }) => (
           <MultiSelect
-            options={options}
+            options={options.map(({ name, _id }) => ({ label: name, value: _id, name: name }))}
             value={value ? value : []}
-            onChange={onChange}
+            onChange={(selected) => {
+              const selectedValues = selected.map((option) => option.id);
+              const newValue = data.filter((option) => {
+                return selectedValues.includes(option.id);
+              });
+       
+              
+              onChange(newValue);
+            }}
             labelledBy="Select"
             disableSearch
             hasSelectAll={false}
           />
-          )}
+        )}
       />
-          <button type="submit">TEST</button>
+      <button type="submit">TEST</button>
     </form>
   );
 }
