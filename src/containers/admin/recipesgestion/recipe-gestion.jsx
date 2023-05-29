3c213f74b0b7f073d664c6c28f75289edaed39b4
Nicolas D'Addabbo <nico.daddabbo7100@gmail.com>
2023-05-29 19:52:26 +0200

    ajout pagination pour gestion recipe admin

--- src/containers/admin/recipesgestion/recipe-gestion.jsx
+++ src/containers/admin/recipesgestion/recipe-gestion.jsx
@@ -1,23 +1,74 @@
-import { getAll } from "../../../../API/recipe"
+import { useInfiniteQuery } from "react-query";
+import { getAll } from "../../../../API/recipe";
 import RecipeAdmin from "../../../components/recipe/recipe-admin";
+import axios from "axios";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 export default function RecipeGestion() {
-    const {data, isError, isLoading} = getAll()
-
-    if (isLoading) {
-        return <p>Wait...</p>
+    const { ref, inView } = useInView();
+  // const {data, isError, isLoading} = getAll()
+  const {
+    status,
+    isLoading,
+    isError,
+    data,
+    error,
+    isFetching,
+    isFetchingNextPage,
+    isFetchingPreviousPage,
+    fetchNextPage,
+    fetchPreviousPage,
+    hasNextPage,
+    hasPreviousPage,
+  } = useInfiniteQuery(
+    ["Recipes"],
+    async ({ pageParam = 0 }) => {
+      const { data } = await axios.get(
+        `http://localhost:8080/api/recipe/admin?page=${pageParam}`
+      );
+      return data;
+    },
+    {
+      getPreviousPageParam: (firstPage) => firstPage.results ?? undefined,
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = allPages.length + 1;
+        return lastPage.results.length !== 0 ? nextPage : undefined;
+      },
     }
-
-    if (isError) {
-        return <p>Error !</p>
+  );
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
     }
-    
-    return (
-        <div className="grid grid-cols-4 gap-4 w-3/4 m-auto mt-32 bg-slate-400">
+  }, [inView]);
 
-         {data.map((recipe) => {
-            return <RecipeAdmin {...recipe} />
-         })}
+  if (isLoading) {
+    return <p>Wait...</p>;
+  }
+
+  if (isError) {
+    return <p>Error !</p>;
+  }
+
+  return (
+    <div className=" w-3/4 m-auto break-words overflow-scroll  bg-slate-400">
+      {/* {data.map((recipe) => {
+        return <RecipeAdmin {...recipe} />;
+      })} */}
+
+      {data.pages.map((page, pageIndex) => (
+        <div className="flex flex-row flex-wrap justify-center w-3/4 h-96 m-auto" key={pageIndex}>
+          {page.results.map((recipe) => (
+            <RecipeAdmin key={recipe.id} {...recipe} />
+          ))}
+          <button
+            ref={ref}
+            onClick={() => fetchNextPage()}
+            disabled={!hasNextPage || isFetchingNextPage}
+          ></button>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
